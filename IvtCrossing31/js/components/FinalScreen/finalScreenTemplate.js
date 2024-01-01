d5c0b49a3dbcbdbed2acbd11dacccbaec13a2d62
RinShams <94722505+RinShams@users.noreply.github.com>
2024-01-01 17:50:17 +0400

    Delete IvtCrossing31 directory

--- IvtCrossing31/js/components/FinalScreen/finalScreenTemplate.js
+++
@@ -1,4 +0,0 @@
-Template.prototype.finalScreenTemplate = () => `
-<div class="finalScreen">    
-    <button id="restart">играть снова</button>
-</div>`
--- IvtCrossing31/js/components/SplashScreen/splashScreenTemplate.js
+++
@@ -1,5 +0,0 @@
-Template.prototype.splashScreenTemplate = () =>`
-<div class="startscreen">
-    <input id="name" placeholder="введи имя" ></input>
-    <button id="play">играть</button>
-</div>`;
