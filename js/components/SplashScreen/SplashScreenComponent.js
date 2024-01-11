8f2272e65a46433afe4f5b7177e4a9dc23588671
evoxed <nikita.gomozov@nutnet.ru>
2024-01-11 22:23:08 +0400

    Add background music and update IDE files
    Updated IDE XML files to adapt the project environment. Added background music to the game to enhance the user experience. Changes made in the game logic were mostly formatting, indentation, or removal of unnecessary white spaces, with no significant alterations to the codebase. Enhanced CSS files to improve game appearance.
    
    (cherry picked from commit 56843a641bcc3a618bef2472ee485445e5e65220)

--- js/components/SplashScreen/SplashScreenComponent.js
+++ js/components/SplashScreen/SplashScreenComponent.js
@@ -6,12 +6,15 @@ class SplashScreenComponent extends Component {
     _addEventListeners() {
         document.getElementById('play').addEventListener('click', () =>
         { //localStorage.clear();
+            const audio = document.querySelector('.audio')
             const name = document.getElementById('name').value;
             if(name) {
                 localStorage.setItem(`lastTry`, `${name}`);
-
+                audio.volume = 0.2
+                audio.play();
                 this.hide(this.id);
                 this.show('game')
+
             } else { console.log('nope')}
         })
     }
