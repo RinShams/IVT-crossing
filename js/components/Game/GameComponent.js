e9163d4b8cdde09e2e3a5127e42da0afcdc7f175
evoxed <nikita.gomozov@nutnet.ru>
2024-01-11 22:28:09 +0400

    Revert "Add background music and update IDE files"
    This reverts commit 56843a641bcc3a618bef2472ee485445e5e65220.

--- js/components/Game/GameComponent.js
+++ js/components/Game/GameComponent.js
@@ -1,14 +1,14 @@
 class GameComponent extends Component {
-    constructor (options) {
-        super(options);
+    constructor (options) {        
+        super(options); 
 
         this.gameScreen = document.getElementById('gameScreen');
         this.rulesScreen = document.getElementById('rulesScreen');
-
-        this.personages = document.querySelectorAll('[data-age]');
-        this.boat = document.querySelector('.boat');
-
-        this.persInBoat = 0;
+        
+        this.personages = document.querySelectorAll('[data-age]');  
+        this.boat = document.querySelector('.boat');        
+        
+        this.persInBoat = 0; 
         this.persOnRightSide = 0;
 
         this.moves = 0;
@@ -18,28 +18,28 @@ class GameComponent extends Component {
         this.isRunning = false;
         this.interval;
 
-        this.game();
+        this.game();  
     }
 
     game() {
-
+        
         document.getElementById('rulesButton').addEventListener('click', () => {
                 this.showRules();
             });
 
-        this.rulesScreen.addEventListener('click', () => {
-                this.closeRules();
+        this.rulesScreen.addEventListener('click', () => {           
+                this.closeRules();                               
             });
 
-        document.getElementById('go').addEventListener('click', () => this.check());
+        document.getElementById('go').addEventListener('click', () => this.check());  
 
         this.personages.forEach(pers => {
-                pers.addEventListener('click', () => this.movePers(pers));
-            });
+                pers.addEventListener('click', () => this.movePers(pers));            
+            });        
     }
 
     showRules() {
-        if ((this.rulesScreen.classList.contains("hide")) && (!this.gameScreen.classList.contains('screenHover'))) {
+        if ((this.rulesScreen.classList.contains("hide")) && (!this.gameScreen.classList.contains('screenHover'))) {            
             this.rulesScreen.classList.remove("hide");
             this.gameScreen.classList.add('screenHover');
             this.pauseTimer();
@@ -57,13 +57,13 @@ class GameComponent extends Component {
     startTimer() {
         if (!this.isRunning) {
           this.isRunning = true;
-          this.interval = setInterval(() => this.updateTimer(), 1000);
-        }
+          this.interval = setInterval(() => this.updateTimer(), 1000); 
+        } 
     }
 
     updateTimer() {
         this.seconds +=1;
-
+        
         if (this.seconds == 60) {
           this.seconds = 0;
           this.minutes +=1;
@@ -87,14 +87,14 @@ class GameComponent extends Component {
             let children = document.querySelectorAll('[data-age="child"]');
             let adults = document.querySelectorAll('[data-age="adult"]');
             let check = true;
-
+            
             children.forEach(child => {
                 let isAdult = false;
                 let myAdult = false;
 
-                adults.forEach(adult => {
-
-                    if (child.dataset.position == adult.dataset.position) {
+                adults.forEach(adult => {  
+                    
+                    if (child.dataset.position == adult.dataset.position) {                    
                         isAdult = true;
                         if (child.dataset.type == adult.dataset.type) {
                             myAdult = true;
@@ -110,48 +110,48 @@ class GameComponent extends Component {
                 this.moves += 1;
                 document.querySelector('#movesValue').textContent = `☘️ ходы: ${this.moves} `;
             } else {
-                this.finishGame('loss');
-            }
-         }
+                this.finishGame('loss'); 
+            } 
+         }       
     }
 
     movePers(pers) {
-        if (pers.dataset.position == 'inBoat') {
+        if (pers.dataset.position == 'inBoat') {            
             this.moveOnBeach(pers);
         } else if (this.persInBoat < 2) {
             this.moveIntoBoat(pers);
         } else {
             this.jump(pers);
-        }
+        }    
     }
 
-    moveOnBeach(pers) {
+    moveOnBeach(pers) {                         
         document.querySelector("#game").appendChild(pers);
         this.persInBoat -=1;
-        if (this.boat.classList.contains('boatRight')) {
-            pers.dataset.position = 'rightSide';
+        if (this.boat.classList.contains('boatRight')) {            
+            pers.dataset.position = 'rightSide';            
             this.persOnRightSide +=1;
             if (this.persOnRightSide == 6) { this.finishGame('win'); }
         } else {
-            pers.dataset.position ='leftSide';
+            pers.dataset.position ='leftSide';            
         }
     }
 
-    moveIntoBoat(pers) {
-        if ( (this.boat.classList.contains('boatRight') && (pers.dataset.position == 'rightSide')) ||
+    moveIntoBoat(pers) {  
+        if ( (this.boat.classList.contains('boatRight') && (pers.dataset.position == 'rightSide')) || 
             ((!this.boat.classList.contains('boatRight')) && (pers.dataset.position == 'leftSide'))) {
-
+             
             if (this.boat.classList.contains('boatRight')) {
                     this.persOnRightSide -=1;
-                }
+                }  
             this.persInBoat +=1;
-            pers.dataset.position = 'inBoat'
-            this.boat.appendChild(pers);
+            pers.dataset.position = 'inBoat'  
+            this.boat.appendChild(pers); 
        } else {
         this.jump(pers);
        }
     }
-
+    
     jump(pers) {
         if (!pers.classList.contains('jump')) {
             pers.classList.add('jump');
@@ -169,12 +169,11 @@ class GameComponent extends Component {
             this.boat.classList.add('boatRight');
         }
     }
-
+    
     finishGame(result) {
-        this.pauseTimer();
-        const audio = document.querySelector('.audio')
-        audio.pause();
-        if (result == 'win') {
+        this.pauseTimer();                 
+
+        if (result == 'win') { 
             document.querySelector('#finalScreen').classList.add('win');
 
             if (this.seconds < 10) {
@@ -183,7 +182,7 @@ class GameComponent extends Component {
                 this.time = `${this.minutes}.${this.seconds}`;
             }
             let name = localStorage.getItem(`lastTry`);
-            localStorage.setItem(`lastTry`, `${name} ${this.time} ${this.moves}`);
+            localStorage.setItem(`lastTry`, `${name} ${this.time} ${this.moves}`);  
 
             let tryNumber = localStorage.getItem('try')-0;
             if (!tryNumber) {
@@ -192,7 +191,7 @@ class GameComponent extends Component {
                 tryNumber +=1;
                 localStorage.setItem(`try`, `${tryNumber}`);
             }
-            localStorage.setItem(`try-${tryNumber}`, `${name} ${this.time} ${this.moves}`);
+            localStorage.setItem(`try-${tryNumber}`, `${name} ${this.time} ${this.moves}`);           
 
         } else if (result == 'loss') {
             document.querySelector('#finalScreen').classList.add('loss');
--- js/components/SplashScreen/SplashScreenComponent.js
+++ js/components/SplashScreen/SplashScreenComponent.js
@@ -6,15 +6,12 @@ class SplashScreenComponent extends Component {
     _addEventListeners() {
         document.getElementById('play').addEventListener('click', () =>
         { //localStorage.clear();
-            const audio = document.querySelector('.audio')
             const name = document.getElementById('name').value;
             if(name) {
                 localStorage.setItem(`lastTry`, `${name}`);
-                audio.volume = 0.2
-                audio.play();
+
                 this.hide(this.id);
                 this.show('game')
-
             } else { console.log('nope')}
         })
     }
